20b1755f0f70c19b04ddf34ebb1b6ea9910a4c1a
Nicolas <nikoalegre@hotmail.com>
2020-07-21 20:43:27 -0300

    Added an icon check and a better front for the Login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import {Container, Button, FormGroup, Label, Input, Form} from 'reactstrap';
+import {Container, Button, FormGroup, Label, Input, Form, Card, Row} from 'reactstrap';
+import { BsCheckCircle } from "react-icons/bs";
+
+
 
 
 const Login = () =>{
     return(
         <Container fluid>
-            <Form>
-                <FormGroup>
-                    <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="examplePassword">Password</Label>
-                    <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-                </FormGroup>
-                <Button color="primary">Submit</Button>
-            </Form>
+            <Row className='justify-content-center'>
+                <Card style={{width:'50%'}}>
+                    <Form>
+                        <FormGroup>
+                            <Label>Enter a new password for 'correo@correo.com'</Label><br/>
+                            <Label>Make sure to include at least:</Label><br/>
+                            <p><i style={{fontSize:25}}><BsCheckCircle/></i> 8 Characters</p>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label>New Password</Label>
+                            <Input type="password" name="password1" id="password1" placeholder="New password" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="examplePassword2">Confirm New Password</Label>
+                            <Input type="password" name="password2" id="password2" placeholder="Confirm new password" />
+                        </FormGroup>
+                        <Row className='justify-content-center'>
+                            <Button style={{width:'80%'}} color="primary">Submit</Button>
+                        </Row>
+                    </Form>
+                </Card>
+            </Row>
         </Container>
   )
 }
