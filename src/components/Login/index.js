186f4f58a0cc1d3ea61a421f301fac5e6060fe5c
Nicolas <nikoalegre@hotmail.com>
2020-07-21 19:35:30 -0300

    Added the componentes folder, the Login component and the path

---
+++ src/components/Login/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container} from 'reactstrap';
+
+
+const Login = () =>{
+    return(
+        <Container fluid className="App text-justify-center">
+            <h1>LOGIN</h1>
+        </Container>
+  )
+}
+
+export default Login;
