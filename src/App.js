a8f867fb22d333f9152108e02ed908df051b57e4
alanfiz <alanfiz92@gmail.com>
2020-07-26 12:44:16 -0300

    Add SelectCountry and VerificationCode components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
 
 import Login from "./components/Login";
-
+import SelectCountry from "./components/SelectCountry";
+import VerificationCode from "./components/VerificationCode";
 
 const App = () => 
   <BrowserRouter>
     <Switch>
       <Route exact path="/Login" component={Login} />
 
+      <Route  path="/VerificationCode" component={VerificationCode} />
+      <Route  path="/SelectCountry" component={SelectCountry} />
 
  
       <Redirect to="/Login" />
