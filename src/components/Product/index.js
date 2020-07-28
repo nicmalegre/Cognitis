11b20c6b5cec578d84c0818e0d64a745e21a345f
cesar acosta <cesaracosta1030@gmail.com>
2020-07-28 14:14:28 -0300

    format document

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -24,204 +24,197 @@ class Product extends React.Component {
     super(props);
     this.state = {
       c1: false,
-      c2:false,
+      c2: false,
       c3: false,
-      product:'',
+      product: "",
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleInputChangeOne = this.handleInputChangeOne.bind(this);
     this.handleInputChangetwo = this.handleInputChangetwo.bind(this);
     this.handleProduct = this.handleProduct.bind(this);
-    
   }
 
-  handleProduct(e){
-    e.preventDefault();
+  handleProduct(e) {
     console.log(this.state.c1);
-    if (this.state.c1){
-      console.log("dentro");
+    if (this.state.c1) {
       this.setState({
         product: 0,
       });
-      
-    }
-    else if (this.state.c2){
+    } else if (this.state.c2) {
       this.setState({
         product: 1,
-      }); 
-    }
-    else {
+      });
+    } else {
       this.setState({
         product: 2,
-      }); 
+      });
     }
-    const product = this.state.product
-    console.log(product);
-    axios.post("http://localhost:4000/api/verificationcode", product);
+    const product = this.state.product;
+    //axios.post("http://localhost:4000/api/verificationcode", product);
   }
 
   handleInputChange(event) {
     const target = event.target;
-    const value = target.name === 'c1' ? target.checked : target.value;
+    const value = target.name === "c1" ? target.checked : target.value;
     const name = target.name;
 
-    if(name == "c1"){
+    if (name == "c1") {
       this.setState({
         c1: value,
         c2: value,
         c3: value,
-        product: '',
+        product: "",
       });
     }
-   
   }
 
   handleInputChangeOne(event) {
     const target = event.target;
     const value = target.name === "c2" ? target.checked : target.value;
     const name = target.name;
-    console.log(value);
-    if ((value) && (this.state.c3)){
+    if (value && this.state.c3) {
       this.setState({
         c1: value,
-        c2:value,
+        c2: value,
       });
-    }
-    else if((value==false) && (this.state.c3)){
+    } else if (value == false && this.state.c3) {
       this.setState({
         c1: value,
-        c2:value,
+        c2: value,
+      });
+    } else {
+      this.setState({
+        [name]: value,
       });
     }
-      else{
-        this.setState({
-          [name]: value,
-        });
-      }
+  }
+
+  handleInputChangetwo(event) {
+    const target = event.target;
+    const value = target.name === "c3" ? target.checked : target.value;
+    const name = target.name;
+
+    if (value && this.state.c2) {
+      this.setState({
+        c1: value,
+        c3: value,
+      });
+    } else if (value == false && this.state.c2) {
+      this.setState({
+        c1: value,
+        c3: value,
+      });
+    } else {
+      this.setState({
+        [name]: value,
+      });
     }
-   
-  handleInputChangetwo(event,) {
-      const target = event.target;
-      const value = target.name === "c3" ? target.checked : target.value;
-      const name = target.name;
-      
-      if ((value) && (this.state.c2)){
-        this.setState({
-          c1: value,
-          c3:value,
-        });
-      }
-      else if((value==false) && (this.state.c2)){
-        this.setState({
-          c1: value,
-          c3:value,
-        });
-      }
-        else{
-          this.setState({
-            [name]: value,
-          });
-        }
-  
-        console.log(name, value);
-        this.setState({
-          [name]: value,
-        });
-      }
+
+    this.setState({
+      [name]: value,
+    });
+  }
   render() {
-        return (
-          <Container fluid>
-            <Row>
-              <Col lg="6" md="3" xs="10">
-                <Logo />
-              </Col>
-            </Row>
-            <Row className="text-center" style={{ marginBottom: 50 }}>
-              <Col lg="12" xs="12">
-                <Welcome />
-              </Col>
-            </Row>
-            <Row>
-              <Col lg="8" sm={{ size: 8, offset: 2 }}>
-                <h3 className="text-center text">
-                  Please select the product to install
-                </h3>
-                <Card id="card-product" md="12" body style={{ marginTop: 50 }}>
-                  <FormGroup row className="item">
-                    <Label
-                      for="checkbox1"
-                      md={10}
-                      className=" text-product text-center h1"
-                    >
-                      Cognitis 360
-                    </Label>
-                    <Col md={{ size: 2 }} className="text-center">
-                      <CustomInput
-                        type="checkbox"
-                        id="checkbox1"
-                        name="c1"
-                        checked={this.state.c1}
-                        onChange={this.handleInputChange}
-                      />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row className="item">
-                    <Label
-                      for="checkbox2"
-                      md={10}
-                      className=" text-product text-center h1"
-                    >
-                      Cognitis Marketplace
-                    </Label>
-                    <Col md={{ size: 2 }} className="text-center">
-                      <CustomInput
-                        type="checkbox"
-                        id="checkbox2"
-                        name="c2"
-                        checked={this.state.c2}
-                        onChange={this.handleInputChangeOne}
-                      />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row className="item">
-                    <Label
-                      for="checkbox3"
-                      md={10}
-                      className=" text-product text-center h1"
-                    >
-                      Cognitis ERP
-                    </Label>
-                    <Col md={{ size: 2 }} className="text-center">
-                      <CustomInput
-                        type="checkbox"
-                        id="checkbox3"
-                        name="c3"
-                        checked={this.state.c3}
-                        onChange={this.handleInputChangetwo}
-                      />
-                    </Col>
-                  </FormGroup>
-                </Card>
-              </Col>
-            </Row>
-            <Row>
-              <Col
-                lg="12"
-                sm={{ size: 8, offset: 2 }}
-                className="d-flex justify-content-center"
-                style={{ marginTop: 20 }}
+    return (
+      <Container fluid>
+        <Row>
+          <Col lg="6" md="3" xs="10">
+            <Logo />
+          </Col>
+        </Row>
+        <Row className="text-center" style={{ marginBottom: 50 }}>
+          <Col lg="12" xs="12">
+            <Welcome />
+          </Col>
+        </Row>
+        <Row>
+          <Col lg="8" sm={{ size: 8, offset: 2 }}>
+            <h3 className="text-center text">
+              Please select the product to install
+            </h3>
+            <Card id="card-product" md="12" body style={{ marginTop: 50 }}>
+              <FormGroup row className="item">
+                <Label
+                  for="checkbox1"
+                  md={10}
+                  className="text-product text-center h1"
+                  style={{ fontSize: 20 }}
+                >
+                  Cognitis 360
+                </Label>
+                <Col md={{ size: 2 }} className="text-center">
+                  <CustomInput
+                    type="checkbox"
+                    id="checkbox1"
+                    name="c1"
+                    checked={this.state.c1}
+                    onChange={this.handleInputChange}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row className="item">
+                <Label
+                  for="checkbox2"
+                  md={10}
+                  className="text-product text-center h1"
+                  style={{ fontSize: 20 }}
+                >
+                  Cognitis Marketplace
+                </Label>
+                <Col md={{ size: 2 }} className="text-center">
+                  <CustomInput
+                    type="checkbox"
+                    id="checkbox2"
+                    name="c2"
+                    checked={this.state.c2}
+                    onChange={this.handleInputChangeOne}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row className="item">
+                <Label
+                  for="checkbox3"
+                  md={10}
+                  className=" text-product text-center h1"
+                  style={{ fontSize: 20 }}
+                >
+                  Cognitis ERP
+                </Label>
+                <Col md={{ size: 2 }} className="text-center">
+                  <CustomInput
+                    type="checkbox"
+                    id="checkbox3"
+                    name="c3"
+                    checked={this.state.c3}
+                    onChange={this.handleInputChangetwo}
+                  />
+                </Col>
+              </FormGroup>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col
+            lg="12"
+            sm={{ size: 8, offset: 2 }}
+            className="d-flex justify-content-center"
+            style={{ marginTop: 20 }}
+          >
+            <Link to="/user">
+              <Button
+                onClick={this.handleProduct}
+                color="warning"
+                style={{ borderColor: "black" }}
               >
-                <Link to="/user" >
-                  <Button onClick={this.handleProduct} color="warning" style={{ borderColor: "black" }}>
-                    <b>Next</b>
-                  </Button>
-                </Link>
-              </Col>
-            </Row>
-          </Container>
-        );
-    }
+                <b>Next</b>
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default Product;
