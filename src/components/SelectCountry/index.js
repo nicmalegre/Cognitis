f883d2b5288bb0ebc69c41f89faec15256b6ca67
Cesar Acosta <37489615+CesarAcosta13@users.noreply.github.com>
2020-07-29 13:25:01 -0300

    done var global

--- src/components/SelectCountry/index.js
+++ src/components/SelectCountry/index.js
@@ -7,13 +7,12 @@
 //Libraries and components imported to use in this component.
 import React from 'react'; 
 import { FormGroup, Label, Col, Button, Input,Row, Container, Card} from 'reactstrap'; 
-import axios from 'axios';
+
 
 import "./selectcountry.css"; 
 
 import Logo from '../base/logo';
 import Welcome from '../base/welcome';
-import axios from "axios";
 
 
 const SelectCountry = (props) => {
@@ -51,7 +50,7 @@ const SelectCountry = (props) => {
                     <FormGroup row>
                             <Label for="exampleSelect" sm={3}>Select Country</Label>
                             <Col sm={7}>
-                                <Input type="select" name="select" id="exampleSelect" onChange={handleInputChange}>
+                                <Input type="select" name="select" id="exampleSelect" onChange={inputChange}>
                                     
                                     {/* Function to insert the countries of the array like items in dropdown menu */}
                                     {countries.map( country => 
