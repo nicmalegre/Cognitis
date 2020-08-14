6512a3c16197cc999fc77520a133749841fd0b48
Nicolas <nikoalegre@hotmail.com>
2020-08-13 22:09:54 -0300

    Updates

---
+++ precache-manifest.14af05e1d750c328b213342aff7717b5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00ce5073965accec0977dcb0784e18a",
+    "url": "/Cognitis-Frontend/index.html"
+  },
+  {
+    "revision": "f10a6c8cd2af69f19f4f",
+    "url": "/Cognitis-Frontend/static/css/2.62f9eacf.chunk.css"
+  },
+  {
+    "revision": "f9613fc9c1084d9dab8a",
+    "url": "/Cognitis-Frontend/static/css/main.20d27a6c.chunk.css"
+  },
+  {
+    "revision": "f10a6c8cd2af69f19f4f",
+    "url": "/Cognitis-Frontend/static/js/2.99d77cf4.chunk.js"
+  },
+  {
+    "revision": "cd1a5d2acd8c7325f6b0f1e7d9a3f82e",
+    "url": "/Cognitis-Frontend/static/js/2.99d77cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9613fc9c1084d9dab8a",
+    "url": "/Cognitis-Frontend/static/js/main.0e4f1e23.chunk.js"
+  },
+  {
+    "revision": "a6b5299ccff03e120fd1",
+    "url": "/Cognitis-Frontend/static/js/runtime-main.48fd6a4f.js"
+  },
+  {
+    "revision": "d200e16bf4b74dc092100f4811f96a35",
+    "url": "/Cognitis-Frontend/static/media/1.d200e16b.jpg"
+  },
+  {
+    "revision": "7d64c61646b9b6b2c138e8cf1f9ea738",
+    "url": "/Cognitis-Frontend/static/media/cognitis-logo_2020.7d64c616.png"
+  },
+  {
+    "revision": "a9e0a6913372b966cc3994bd2381fde8",
+    "url": "/Cognitis-Frontend/static/media/cognitisLogo-Texto.a9e0a691.png"
+  },
+  {
+    "revision": "28f9cad9246e4adc6b148f589ec5f9be",
+    "url": "/Cognitis-Frontend/static/media/companylogo.28f9cad9.png"
+  },
+  {
+    "revision": "e57d1676ed25b39aa1dc5ad1fafa8f5f",
+    "url": "/Cognitis-Frontend/static/media/imageProductivity.e57d1676.jpeg"
+  },
+  {
+    "revision": "f642fc6044e4ab31574255c395d34750",
+    "url": "/Cognitis-Frontend/static/media/iphone.f642fc60.jpg"
+  },
+  {
+    "revision": "cd8164e4d7d105d95fe1bea1e9b1cabd",
+    "url": "/Cognitis-Frontend/static/media/iphone2.cd8164e4.jpg"
+  },
+  {
+    "revision": "5eba7e23b85f86eb33c7bced7357c4aa",
+    "url": "/Cognitis-Frontend/static/media/iphone3.5eba7e23.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Cognitis-Frontend/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Cognitis-Frontend/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Cognitis-Frontend/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Cognitis-Frontend/static/media/slick.f97e3bbf.svg"
+  }
+]);
