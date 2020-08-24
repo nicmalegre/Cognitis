8bb8896c36830a8b78956a4e0d378ac335d562ab
unknown <nicoromero3210@gmail.com>
2020-08-24 02:48:22 -0300

    Fix url herokku

--- src/urls/url.js
+++ src/urls/url.js
@@ -1,7 +1,7 @@
 //URL PARA LOCAL
-export const BASE_URL = 'http://localhost:3000/api';
+//export const BASE_URL = 'http://localhost:3000/api';
 
 //URL PARA HEROKKU
-//export const BASE_URL = 'https://cognitis-360.herokuapp.com/api';
+export const BASE_URL = 'https://cognitis-360.herokuapp.com/api';
 
 export const PRODUCTS_URL = `${BASE_URL}/products`;
