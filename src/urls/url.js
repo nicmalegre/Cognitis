cd7d574879ed92a4a91e8667daa82f31f3e1f2ae
Sebastian Rodriguez <sebasrz.rcia@gmail.com>
2020-08-29 18:50:56 -0300

    add all routes to url.js file

--- src/urls/url.js
+++ src/urls/url.js
@@ -7,3 +7,17 @@ export const BASE_URL = 'http://localhost:4000/api';
 export const PRODUCTS_URL = `${BASE_URL}/products`;
 
 export const CATEGORIES_URL = `${BASE_URL}/categories`;
+
+export const COMPANIES_URL = `${BASE_URL}/company`;
+
+export const BRANCHOFFIECHOUSE_URL = `${BASE_URL}/branchofficehouse`;
+
+export const HEAD_HOUSE_URL = `${BASE_URL}/head_house`;
+
+export const INDUSTRY_URL = `${BASE_URL}/industry`;
+
+export const USERS_URL = `${BASE_URL}/users`;
+
+export const VERIFICATIONCODE = `${BASE_URL}/verificationcode`;
+
+
